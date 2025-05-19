b77aa8a335582f5261efdb38bbafcf6bc96da3a8
jhoanchq <jhoan.chiteq@gmail.com>
2025-05-18 22:38:29 -0500

    main

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const QRPortalWeb = require('@bot-whatsapp/portal')
 const BaileysProvider = require('@bot-whatsapp/provider/baileys')
 const MockAdapter = require('@bot-whatsapp/database/mock')
 
-
-
+// Flujo principal con solo un mensaje
 const flowPrincipal = addKeyword(['hola', 'ole', 'alo'])
 .addAnswer(
-    '🙌 Hola, bienvenido a este *Curso de Chatbots con Node.js*! '
+    '🙌 Hola, bienvenido a este *Curso de desarrollo de Chatbot con Node.js*! '
 )
+
+
 const main = async () => {
     const adapterDB = new MockAdapter()
     const adapterFlow = createFlow([flowPrincipal])
